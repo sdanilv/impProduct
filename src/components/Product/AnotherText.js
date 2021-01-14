227e8d964130d83bd640a36cdb7cf088406488f1
DivinusIocus <DivinusIocus@MDIVINUSIOCUS>
2021-01-14 14:06:17 +0200

    add move AnotherText css

--- src/components/Product/AnotherText.js
+++ src/components/Product/AnotherText.js
@@ -1,5 +1,5 @@
 import React from "react";
-import classes from "./Product.module.css";
+import classes from "./AnotherText.module.css";
 import {Link} from "react-router-dom";
 
 const AnotherText = () => (
