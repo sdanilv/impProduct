f0fe5191743f69b5d0e7f7a0953c2924b765b3a8
DivinusIocus <DivinusIocus@MDIVINUSIOCUS>
2021-01-14 14:06:56 +0200

    add cash control to api

--- src/api.js
+++ src/api.js
@@ -1,14 +1,24 @@
-
 const fetchData = async (url, method = "GET", data) => {
-const response = await fetch(`https://imp-product-server.herokuapp.com/shops/5ff9ab4b6aa5fa31a4f23783/${url}`, {
-    method,
-    mode: "cors",
-    headers: {
-        "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-});
-return await response.json();}
+  try {
+    const response = await fetch(
+      `https://imp-product-server.herokuapp.com/shops/5ff9ab4b6aa5fa31a4f23783/${url}`,
+      {
+        method,
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+          "Cache-Control": "max-age=31536000",
+        },
+        body: JSON.stringify(data),
+      }
+    );
+    return await response.json();
+  } catch (except) {
+    console.log(except);
+    return [];
+  }
+};
 
-export const generateSalt = async  (data) => await fetchData(`products/salt`, "POST", data)
-export const getAllProducts = async  () => await fetchData(`products`)
+export const generateSalt = async (data) =>
+  await fetchData(`products/salt`, "POST", data);
+export const getAllProducts = async () => await fetchData(`products`);
